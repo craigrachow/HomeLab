257cd4026d66771c5d8b95fbdca9a6b18cd11726
craigrachow <78630624+craigrachow@users.noreply.github.com>
2023-12-02 22:06:37 +0930

    Update prometheus-grafana.sh

--- prometheus-grafana.sh
+++ prometheus-grafana.sh
@@ -3,17 +3,18 @@
 echo "Make Container for Prometheus & Grafana Machine"
 sudo mkdir -pv ./Containers/prometheus-grafana
 
+echo "Create Prometheus & Grafana compose.yaml"
 echo "services:
   prometheus:
     image: prom/prometheus
     container_name: prometheus
     command:
-      - '--config.file=/etc/prometheus/prometheus.yml'
+      - '--config.file=/etc/prometheus/prometheus.yaml'
     ports:
       - 9090:9090
     restart: unless-stopped
     volumes:
-      - ./prometheus:/etc/prometheus
+      - ./:/etc/prometheus
       - prom_data:/prometheus
   grafana:
     image: grafana/grafana
@@ -25,8 +26,41 @@ echo "services:
       - GF_SECURITY_ADMIN_USER=admin
       - GF_SECURITY_ADMIN_PASSWORD=grafana
     volumes:
-      - ./grafana:/etc/grafana/provisioning/datasources
+      - ./:/etc/grafana/provisioning/datasources
 volumes:
   prom_data:" | sudo tee -a ./Containers/prometheus-grafana/compose.yaml > /dev/null 
 
-  
+
+  echo "Create Grafana datasource.yaml"
+  echo "apiVersion: 1
+
+datasources:
+- name: Prometheus
+  type: prometheus
+  url: http://prometheus:9090 
+  isDefault: true
+  access: proxy
+  editable: true" | sudo tee -a ./Containers/prometheus-grafana/datasource.yaml > /dev/null 
+
+   echo "Create prometheus.yaml"
+   echo "global:
+  scrape_interval: 15s
+  scrape_timeout: 10s
+  evaluation_interval: 15s
+alerting:
+  alertmanagers:
+  - static_configs:
+    - targets: []
+    scheme: http
+    timeout: 10s
+    api_version: v1
+scrape_configs:
+- job_name: prometheus
+  honor_timestamps: true
+  scrape_interval: 15s
+  scrape_timeout: 10s
+  metrics_path: /metrics
+  scheme: http
+  static_configs:
+  - targets:
+    - localhost:9090" | sudo tee -a ./Containers/prometheus-grafana/prometheus.yaml > /dev/null 
