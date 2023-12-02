68bb5399bdbe52101401119b31bb949ce8c9c4f0
craigrachow <78630624+craigrachow@users.noreply.github.com>
2023-12-02 19:41:00 +0930

    Update prepserver.sh

--- prepserver.sh
+++ prepserver.sh
@@ -1,11 +1,13 @@
 # This file prepares a linux server for homelab deployment.
-# Endure this is executed in the correct directory. eg /HomeLab
+# Ensure this is executed in the correct directory. eg /HomeLab. 
+# git pull https://github.com/craigrachow/HomeLab
 
 echo "starting prepserver.sh script"
 
 # Update OS and Repo's
+apt-get install docker-cli
 apt-get install docker.io
-#apt-get install docker-cli
+
 
 # Create Software/Storage/Containers Directories - maybe not needed as the git pull should create
 
