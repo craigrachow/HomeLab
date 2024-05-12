84109b3aea53220ca8f036063bcc1b301ef77b21
craigrachow <78630624+craigrachow@users.noreply.github.com>
2024-05-12 18:21:05 +0930

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -1,13 +1,21 @@
 # instructions from here https://github.com/docker/awesome-compose/tree/master/portainer
 
 echo "Make Container for Portainer"
-sudo mkdir -pv ./Containers/portainer
+sudo mkdir -pv ./Containers/portainer/portainer_data
 
 
 echo "Create Portainer compose.yaml"
 echo "services:
   portainer:
-    image: portainer/portainer-ce:alpine" | sudo tee -a ./Containers/portainer/compose.yaml > /dev/null 
+    image: portainer/portainer-ce:alpine
+    container_name: prometheus
+    ports:
+      - 9000:9000
+      - 8000:8000
+      - 9443:9443
+    restart: unless-stopped
+    volumes:
+      - ./portainer_data:/etc/portainer_data" | sudo tee -a ./Containers/portainer/compose.yaml > /dev/null 
 
 echo "Start Container for Portainer Machine"    
 sudo docker-compose -f ./Containers/portainer/compose.yaml up -d
