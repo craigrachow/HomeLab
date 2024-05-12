bd222883ef737da5bdd4afb8537dc8403b4f5937
craigrachow <78630624+craigrachow@users.noreply.github.com>
2024-05-12 17:52:51 +0930

    Update portainer.sh

--- portainer.sh
+++ portainer.sh
@@ -1,1 +1,14 @@
+# instructions from here https://github.com/docker/awesome-compose/tree/master/portainer
 
+echo "Make Container for Portainer"
+sudo mkdir -pv ./Containers/portainer
+
+
+echo "Create Portainer compose.yaml"
+echo "services:
+  portainer:
+    image: portainer/portainer-ce:alpine" | sudo tee -a ./Containers/portainer/compose.yaml > /dev/null 
+
+echo "Start Container for Portainer Machine"    
+sudo docker-compose -f ./Containers/portainer/compose.yaml up -d
+sudo docker ps
