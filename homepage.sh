75b59c6c5e6871dea2d6b1f49f98b6a45018ef93
craigrachow <78630624+craigrachow@users.noreply.github.com>
2025-04-14 20:22:02 +0930

    Create homepage.sh

---
+++ homepage.sh
@@ -0,0 +1,21 @@
+# instructions from here https://gethomepage.dev/installation/docker/
+
+echo "Make Container for Homepage"
+sudo mkdir -pv ./Containers/homepage/
+
+echo "Create Homepage compose.yaml"
+echo "services:
+  homepage:
+    image: ghcr.io/gethomepage/homepage:latest
+    container_name: homepage
+    ports:
+      - 3000:3000
+    volumes:
+      - /path/to/config:/app/config # Make sure your local config directory exists
+      - /var/run/docker.sock:/var/run/docker.sock # (optional) For docker integrations
+    environment:
+      HOMEPAGE_ALLOWED_HOSTS: gethomepage.dev # required, may need port. See gethomepage.dev/installation/#homepage_allowed_hosts" | sudo tee -a ./Containers/portainer/compose.yaml > /dev/null 
+
+echo "Start Container for Portainer Machine"    
+sudo docker-compose -f ./Containers/portainer/compose.yaml up -d
+sudo docker ps
