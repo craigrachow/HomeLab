a89fe02da6b856214f44204b479c76f8a8d408a9
craigrachow <78630624+craigrachow@users.noreply.github.com>
2025-04-14 20:53:27 +0930

    Update homepage.sh

--- homepage.sh
+++ homepage.sh
@@ -14,8 +14,8 @@ echo "services:
       - ./homepage:/app/config # Make sure your local config directory exists
       - /var/run/docker.sock:/var/run/docker.sock # (optional) For docker integrations
     environment:
-      HOMEPAGE_ALLOWED_HOSTS: * # required, may need port. See gethomepage.dev/installation/#homepage_allowed_hosts
-      PORT: 4000" | sudo tee -a ./Containers/homepage/compose.yaml > /dev/null 
+      HOMEPAGE_ALLOWED_HOSTS=* # required, may need port. See gethomepage.dev/installation/#homepage_allowed_hosts
+      PORT=4000" | sudo tee -a ./Containers/homepage/compose.yaml > /dev/null 
 
 echo "Start Container for Homepage Machine"    
 sudo docker-compose -f ./Containers/homepage/compose.yaml up -d
