d10768d04f1b9727fe046aa660ecb5d68429fff1
craigrachow <78630624+craigrachow@users.noreply.github.com>
2025-04-21 20:23:27 +0930

    Update homarr.sh

--- homarr.sh
+++ homarr.sh
@@ -10,12 +10,12 @@ echo "services:
       container_name: homarr
       image: ghcr.io/homarr-labs/homarr:latest
       restart: unless-stopped
-    ports:
-      - '7575:7575'
-    environment:
-      - SECRET_ENCRYPTION_KEY=cca7b7b2e0997f0c48a8be2cd949ce715f36cc5c8844b42f8cbdaf0ff99307b5
-    volumes:
-      - ./homarr_data:/etc/homarr_data" | sudo tee -a ./Containers/homarr/compose.yaml > /dev/null 
+      ports:
+          - '7575:7575'
+      environment:
+        - SECRET_ENCRYPTION_KEY=cca7b7b2e0997f0c48a8be2cd949ce715f36cc5c8844b42f8cbdaf0ff99307b5
+      volumes:
+        - ./homarr_data:/etc/homarr_data" | sudo tee -a ./Containers/homarr/compose.yaml > /dev/null 
 
 echo "Start Container for Homarr Machine"    
 sudo docker-compose -f ./Containers/homarr/compose.yaml up -d
