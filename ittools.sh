818513ed4f789f1c4972c10611fac610426c7555
craigrachow <78630624+craigrachow@users.noreply.github.com>
2025-04-21 20:39:09 +0930

    Create ittools.sh

---
+++ ittools.sh
@@ -0,0 +1,20 @@
+# instructions from here https://docs.techdox.nz/ittools/
+
+echo "Make Container for ITtools"
+sudo mkdir -pv ./Containers/ittools/ittools_data
+
+
+echo "Create ITtools compose.yaml"
+echo "services:
+    ittools:
+      container_name: ittools
+      image: image: 'corentinth/it-tools:latest'  # The Docker image to use.
+      restart: unless-stopped
+      ports:
+          - '8055:80'
+      volumes:
+        - ./ittools_data:/etc/ittools_data" | sudo tee -a ./Containers/ittools/compose.yaml > /dev/null 
+
+echo "Start Container for ITtools Machine"    
+sudo docker-compose -f ./Containers/ittools/compose.yaml up -d
+sudo docker ps
